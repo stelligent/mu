e862b1690fdedade4bf74b26c4f7f262259c3065
Casey Lee <cplee@nektos.com>
2017-01-11 00:00:47 -0800

    add test coverage to environment commands

--- cli/environments_test.go
+++ cli/environments_test.go
@@ -4,6 +4,11 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"github.com/stelligent/mu/common"
+	"github.com/urfave/cli"
+	"io/ioutil"
+	"flag"
+	"bytes"
+	"github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface"
 )
 
 func TestNewEnvironmentsCommand(t *testing.T) {
@@ -19,12 +24,18 @@ func TestNewEnvironmentsCommand(t *testing.T) {
 	assert.Equal("env", command.Aliases[0], "Aliases should match")
 	assert.Equal("options for managing environments", command.Usage, "Usage should match")
 	assert.Equal(4, len(command.Subcommands), "Subcommands len should match")
+
+	args := []string { "environment","help" }
+	err := runCommand(command, args)
+	assert.Nil(err)
 }
+
 
 func TestNewEnvironmentsUpsertCommand(t *testing.T) {
 	assert := assert.New(t)
 	ctx := common.NewContext()
 	command := newEnvironmentsUpsertCommand(ctx)
+	ctx.CloudFormation = new(mockedCloudFormation)
 
 	assert.NotNil(command)
 	assert.Equal("upsert", command.Name, "Name should match")
@@ -32,6 +43,16 @@ func TestNewEnvironmentsUpsertCommand(t *testing.T) {
 	assert.Equal("up", command.Aliases[0], "Aliases should match")
 	assert.Equal("<environment>", command.ArgsUsage, "ArgsUsage should match")
 	assert.NotNil(command.Action)
+
+	args := []string { "upsert" }
+	err := runCommand(command, args)
+	assert.NotNil(err)
+	assert.Equal(1,lastExitCode)
+
+	args = []string { "upsert","fooenv" }
+	err = runCommand(command, args)
+	assert.NotNil(err)
+	assert.Equal(1,lastExitCode)
 }
 
 func TestNewEnvironmentsListCommand(t *testing.T) {
@@ -67,4 +88,29 @@ func TestNewEnvironmentsTerminateCommand(t *testing.T) {
 	assert.Equal("term", command.Aliases[0], "Aliases should match")
 	assert.Equal("<environment>", command.ArgsUsage, "ArgsUsage should match")
 	assert.NotNil(command.Action)
+}
+
+func runCommand(command *cli.Command, args []string) error {
+	app := cli.NewApp()
+	app.Writer = ioutil.Discard
+	set := flag.NewFlagSet("test", 0)
+	set.Parse(args)
+	appContext := cli.NewContext(app, set, nil)
+	return command.Run(appContext)
+}
+
+var (
+	lastExitCode = 0
+	fakeOsExiter = func(rc int) {
+		lastExitCode = rc
+	}
+	fakeErrWriter = &bytes.Buffer{}
+)
+
+func init() {
+	cli.OsExiter = fakeOsExiter
+	cli.ErrWriter = fakeErrWriter
+}
+type mockedCloudFormation struct {
+	cloudformationiface.CloudFormationAPI
 }
