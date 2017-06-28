4c00fba8e749cbaff35699a924b54b6a7a7589b1
Casey Lee <casey.lee@stelligent.com>
2017-06-27 17:12:44 -0700

    add test for s3 artifact manager

---
+++ provider/aws/s3_test.go
@@ -0,0 +1,38 @@
+package aws
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+)
+
+type mockedS3 struct {
+	mock.Mock
+	s3iface.S3API
+}
+
+func (m *mockedS3) PutObject(*s3.PutObjectInput) (*s3.PutObjectOutput, error) {
+	args := m.Called()
+	return nil, args.Error(1)
+}
+
+
+func TestS3ArtifactManager_CreateArtifact(t *testing.T) {
+	assertion := assert.New(t)
+	s3Mock := new(mockedS3)
+
+	s3Mock.On("PutObject").Return(&s3.PutObjectOutput{}, nil)
+
+
+	artifactManager := s3ArtifactManager{
+		s3API: s3Mock,
+	}
+
+	err := artifactManager.CreateArtifact(nil,"s3://bucket/key")
+	assertion.Nil(err)
+
+	s3Mock.AssertExpectations(t)
+	s3Mock.AssertNumberOfCalls(t, "PutObject", 1)
+}
