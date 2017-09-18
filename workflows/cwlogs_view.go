62141d18f93dc75f979029895b62a83dece86fe3
Judd Montgomery <judd@jpilot.org>
2017-09-18 11:20:11 -0400

    minor space change

--- workflows/cwlogs_view.go
+++ workflows/cwlogs_view.go
@@ -15,6 +15,7 @@ type logsWorkflow struct {
 // NewEnvironmentLogViewer create a new workflow for following logs environments
 func NewEnvironmentLogViewer(ctx *common.Context, searchDuration time.Duration, follow bool, environmentName string, writer io.Writer, filter string) Executor {
 	workflow := new(logsWorkflow)
+
 	logGroup := common.CreateStackName(ctx, common.StackTypeEnv, environmentName)
 
 	return newPipelineExecutor(
@@ -25,6 +26,7 @@ func NewEnvironmentLogViewer(ctx *common.Context, searchDuration time.Duration, 
 // NewServiceLogViewer create a new workflow for following logs for services
 func NewServiceLogViewer(ctx *common.Context, searchDuration time.Duration, follow bool, environmentName string, serviceName string, writer io.Writer, filter string) Executor {
 	workflow := new(logsWorkflow)
+
 	logGroup := common.CreateStackName(ctx, common.StackTypeService, getServiceName(ctx, serviceName), environmentName)
 
 	return newPipelineExecutor(
