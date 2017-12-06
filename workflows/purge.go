76c443e298b6baa64a19abf775d607445193d392
Tim Bailey-Jones <timothy.baileyjones@stelligent.com>
2017-12-06 12:09:06 -0500

    `go fmt` fix

--- workflows/purge.go
+++ workflows/purge.go
@@ -23,55 +23,51 @@ func NewPurge(ctx *common.Context, writer io.Writer) Executor {
 	)
 }
 
+//
+//
+// main.go: main
+// cli/app.go: NewApp
+// cli/environments.go: newEnvironmentsCommand
+// cli/environments.go: newEnvironmentsTerminateCommand
+// workflows/environment_terminate.go: NewEnvironmentTerminate
 
+// main.go: main
+// cli/app.go: NewApp
+// cli/services.go: newServicesCommand
+// cli/services.go: newServicesUndeployCommand
+// workflows/service_undeploy.go: newServiceUndeployer
 
+//Workflow sequence
+//
+//for region in region-list (default to current, maybe implement a --region-list or --all-regions switch)
+//  for namespace in namespaces (default to specified namespace)
+//    for environment in all-environments (i.e. acceptance/production)
+//      for service in services (all services in environment)
+//         invoke 'svc undeploy'
+//         invoke `env term`
+//remove ECS repo
+//invoke `pipeline term`
+//remove s3 bucket containing environment name
+//remove RDS databases
+//
+//other artifacts to remove:
+//* common IAM roles
+//* cloudwatch buckets
+//* cloudwatch dashboards
+//* (should be covered by CFN stack removal)
+//* ECS scheduled tasks
+//* SES
+//* SNS
+//* SQS
+//* ELB
+//* EC2 subnet
+//* EC2 VPC Gateway attachment
+//* security groups
+//* EC2 Network ACL
+//* EC2 Routetable
+//* CF stacks
 
-	//
-	//
-	// main.go: main
-	// cli/app.go: NewApp
-	// cli/environments.go: newEnvironmentsCommand
-	// cli/environments.go: newEnvironmentsTerminateCommand
-	// workflows/environment_terminate.go: NewEnvironmentTerminate
-
-	// main.go: main
-	// cli/app.go: NewApp
-	// cli/services.go: newServicesCommand
-	// cli/services.go: newServicesUndeployCommand
-	// workflows/service_undeploy.go: newServiceUndeployer
-
-		//Workflow sequence
-		//
-		//for region in region-list (default to current, maybe implement a --region-list or --all-regions switch)
-		//  for namespace in namespaces (default to specified namespace)
-		//    for environment in all-environments (i.e. acceptance/production)
-		//      for service in services (all services in environment)
-		//         invoke 'svc undeploy'
-        //         invoke `env term`
-		//remove ECS repo
-		//invoke `pipeline term`
-		//remove s3 bucket containing environment name
-		//remove RDS databases
-		//
-		//other artifacts to remove:
-		//* common IAM roles
-		//* cloudwatch buckets
-		//* cloudwatch dashboards
-		//* (should be covered by CFN stack removal)
-		//* ECS scheduled tasks
-		//* SES
-		//* SNS
-		//* SQS
-		//* ELB
-		//* EC2 subnet
-		//* EC2 VPC Gateway attachment
-		//* security groups
-		//* EC2 Network ACL
-		//* EC2 Routetable
-		//* CF stacks
-
-
-func (workflow *purgeWorkflow) purgeWorker(ctx	*common.Context, stackLister common.StackLister, writer io.Writer) Executor {
+func (workflow *purgeWorkflow) purgeWorker(ctx *common.Context, stackLister common.StackLister, writer io.Writer) Executor {
 	return func() error {
 		// TODO establish outer loop for regions
 		// TODO establish outer loop for multiple namespaces
