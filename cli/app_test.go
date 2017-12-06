80fffa0b5ff6e046c69aa0282c932922a7c449c6
Tim Bailey-Jones <timothy.baileyjones@stelligent.com>
2017-12-06 12:07:40 -0500

    update app unit test to expect 6 subcommands instead of 5

--- cli/app_test.go
+++ cli/app_test.go
@@ -27,7 +27,7 @@ func TestNewApp(t *testing.T) {
 	assert.Equal("disable-iam, I", app.Flags[9].GetName(), "Flags name should match")
 	assert.Equal("skip-version-check, F", app.Flags[10].GetName(), "Flags name should match")
 	assert.Equal("proxy, P", app.Flags[11].GetName(), "Flags name should match")
-	assert.Equal(5, len(app.Commands), "Commands len should match")
+	assert.Equal(6, len(app.Commands), "Commands len should match")
 	assert.Equal("init", app.Commands[0].Name, "Command[0].name should match")
 	assert.Equal("environment", app.Commands[1].Name, "Command[1].name should match")
 	assert.Equal("service", app.Commands[2].Name, "Command[2].name should match")
