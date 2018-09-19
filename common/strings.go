2fa742eb32ea7454d0ca8c8d803d960f9fa87994
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2018-09-18 22:54:08 -0400

    document new fns

--- common/strings.go
+++ common/strings.go
@@ -2,6 +2,8 @@ package common
 
 import "strconv"
 
+// NewStringIfNotEmpty takes strings a and b, and returns a unless
+// string b is not empty.
 func NewStringIfNotEmpty(original string, newString string) string {
 	if newString != "" {
 		return newString
@@ -9,6 +11,8 @@ func NewStringIfNotEmpty(original string, newString string) string {
 	return original
 }
 
+// NewStringIfNotZero takes string a and int b, and returns a unless
+// int b is not zero, in which case it returns Itoa(b).
 func NewStringIfNotZero(original string, newString int) string {
 	if newString != 0 {
 		return strconv.Itoa(newString)
