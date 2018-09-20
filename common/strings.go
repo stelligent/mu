025fa0068cf58cbd940f13e8490c8dee76d2ad1e
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2018-09-20 12:40:59 -0400

    Add new functions for dealing with map elements

--- common/strings.go
+++ common/strings.go
@@ -11,6 +11,22 @@ func NewStringIfNotEmpty(original string, newString string) string {
 	return original
 }
 
+// NewMapElementIfNotEmpty adds a new element to map given by destMap if the
+// value given in newString is not empty
+func NewMapElementIfNotEmpty(destMap map[string]string, destElement string, newString string) {
+	if newString != "" {
+		destMap[destElement] = newString
+	}
+}
+
+// NewMapElementIfNotZero adds a new element to map given by destMap if the
+// value given in newInt is not empty (newInt is converted to string)
+func NewMapElementIfNotZero(destMap map[string]string, destElement string, newInt int) {
+	if newInt != 0 {
+		destMap[destElement] = strconv.Itoa(newInt)
+	}
+}
+
 // NewStringIfNotZero takes string a and int b, and returns a unless
 // int b is not zero, in which case it returns Itoa(b).
 func NewStringIfNotZero(original string, newString int) string {
