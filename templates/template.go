3796a9d5265d059137d58a1a51d00b0f8b0b3f49
Michael Neil <mneil@mneilsworld.com>
2018-09-24 14:37:05 -0700

    Issue #273 - Update template with ToString method

--- templates/template.go
+++ templates/template.go
@@ -22,6 +22,16 @@ func NewPolicy(assetName string) (io.Reader, error) {
 	return getAsset(fmt.Sprintf("policies/%s", assetName), nil)
 }
 
+// TemplateToString takes an io.Reader and converts it to string
+func TemplateToString(reader io.Reader) (string, error) {
+	bodyBytes := new(bytes.Buffer)
+	_, err := bodyBytes.ReadFrom(reader)
+	if err != nil {
+		return "", err
+	}
+	return bodyBytes.String(), err
+}
+
 func getAsset(assetName string, data interface{}) (io.Reader, error) {
 	box := packr.NewBox("./assets")
 
