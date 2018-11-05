74091296fe42b77df442d38ba2f47580c1c1a951
Casey Lee <casey.lee@stelligent.com>
2018-11-04 21:50:07 -0800

    fixes #391 - don't add stack param for database name if empty string

--- provider/aws/roleset.go
+++ provider/aws/roleset.go
@@ -223,7 +223,10 @@ func (rolesetMgr *iamRolesetManager) UpsertServiceRoleset(environmentName string
 		"ServiceName":      serviceName,
 		"Provider":         envProvider,
 		"CodeDeployBucket": codeDeployBucket,
-		"DatabaseName":     databaseName,
+	}
+
+	if databaseName != "" {
+		stackParams["DatabaseName"] = databaseName
 	}
 
 	policy, err := templates.GetAsset(common.TemplatePolicyDefault)
